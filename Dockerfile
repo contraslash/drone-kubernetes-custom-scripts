4468508c18599d3f16201499134a2eed3f1fabb5
Mauricio Collazos <ma0@contraslash.com>
2019-08-14 08:34:11 -0500

    moified script to start with ash

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ RUN apk --no-cache add curl ca-certificates bash
 RUN curl -Lo /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.13.3/bin/linux/amd64/kubectl
 RUN chmod +x /usr/local/bin/kubectl
 RUN apk --no-cache add grep
-COPY update.sh /bin/
+COPY configure_kubectl.sh /bin/
+RUN chmod +x /bin/configure_kubectl.sh && /bin/configure_kubectl.sh
 #ENTRYPOINT ["/bin/bash"]
-CMD ["/bin/update.sh"]
-#CMD ["ash"]
+#CMD ["/bin/update.sh"]
+CMD ["ash"]
--- configure_kubectl.sh
+++ configure_kubectl.sh
@@ -51,19 +51,3 @@ fi
 
 kubectl config set-context default --cluster=default --user=${PLUGIN_KUBERNETES_USER}
 kubectl config use-context default
-
-#kubectl get nodes
-
-kubectl version
-env
-echo "=========="
-kubectl get pods
-echo "=========="
-kubectl get pods --field-selector=status.phase==Running --sort-by=.status.startTime | tail -r
-echo "=========="
-echo kubectl get pods --field-selector=status.phase==Running --sort-by=.status.startTime | tac | grep "${PLUGIN_POD_NAME}" -m 1 |  awk '{print $1}'
-SELECTED_POD=$(kubectl get pods --field-selector=status.phase==Running --sort-by=.status.startTime | tac | grep "${PLUGIN_POD_NAME}" -m 1 | awk '{print $1}')
-echo "${SELECTED_POD}"
-echo "EXECUTING: kubectl exec -it ${SELECTED_POD} -c ${PLUGIN_CONTAINER_NAME} ${PLUGIN_CONTAINER_COMMAND}"
-kubectl exec -it ${SELECTED_POD} -c ${PLUGIN_CONTAINER_NAME} ${PLUGIN_CONTAINER_COMMAND}
-
